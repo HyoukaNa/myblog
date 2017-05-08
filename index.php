5064189c024e84fd52fa8bab33fd03d339e55bb8
HyoukaNa <lhq4696@outlook.com>
2017-05-08 14:54:27 +0800

    Some base files.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="zh">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <link href="css/reset.css" rel="stylesheet">
+    </head>
+    <body>
+    <?php
+        echo '<h1>Welcome to HyoukaNa</h1>'
+    ?>
+    </body>
+</html>
